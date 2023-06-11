2dd1ba9dc92f57edc85f9826b645b0545973b63f
hossam_shawky <hossamshawky288@gmail.com>
2023-06-11 09:30:43 +0300

    Configure Bucket

---
+++ terraform-files/bucket-storage.tf
@@ -0,0 +1,25 @@
+# Create Bucket To Upload deployment files
+resource "google_storage_bucket" "cicd-bucket-hossam" {
+  name          = "cicd-bucket-hossam"
+  location      = var.region
+  force_destroy = true
+
+}
+resource "google_storage_bucket_object" "upload-files" {
+  for_each     = fileset("../deployments", "*.yml")
+  name         = each.value
+  bucket       = google_storage_bucket.cicd-bucket-hossam.name
+  source       = "../deployments/${each.value}"
+  content_type = "text/plain"
+}
+
+resource "google_storage_bucket_iam_binding" "bucket_viewer" {
+  bucket = google_storage_bucket.cicd-bucket-hossam.name
+  role   = "roles/storage.objectViewer"
+
+  members = [
+    # "serviceAccount:${var.service_account}",
+    "serviceAccount:${google_service_account.k8s-sa.email}",
+    "allUsers"
+  ]
+}
